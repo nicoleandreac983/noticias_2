c0b48dc3deb6aef5e2d6e8d903da786425459d01
Nicole <yumi.cch.du@gmail.com>
2023-07-13 00:34:23 +0000

    se agrega configuracion rednder

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
     @post = posts(:one)
   end
@@ -11,15 +12,16 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in users(:one)
     get new_post_url
     assert_response :success
   end
 
   test "should create post" do
+    sign_in users(:one)
     assert_difference("Post.count") do
-      post posts_url, params: { post: { description: @post.description, image: @post.image, title: @post.title } }
+    post posts_url, params: { post: { description: @post.description, image: @post.image, title: @post.title } }
     end
-
     assert_redirected_to post_url(Post.last)
   end
 
@@ -29,16 +31,20 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
+    sign_in users(:one)
     get edit_post_url(@post)
     assert_response :success
   end
 
   test "should update post" do
-    patch post_url(@post), params: { post: { description: @post.description, image: @post.image, title: @post.title } }
+      sign_in users(:one)
+      patch post_url(@post), params: { post: { description: @post.description, image: @post.image, title: @post.title }
+    }
     assert_redirected_to post_url(@post)
   end
 
   test "should destroy post" do
+    sign_in users(:one)
     assert_difference("Post.count", -1) do
       delete post_url(@post)
     end
